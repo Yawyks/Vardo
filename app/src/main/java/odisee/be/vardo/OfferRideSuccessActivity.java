4d5d8705342580bd9af5424a8308bea7c695b90f
yawyks <mohamed.dahhou@student.odisee.be>
2018-05-02 12:41:18 +0200

    Design prototype.

--- app/src/main/java/odisee/be/vardo/OfferRideSuccessActivity.java
+++ app/src/main/java/odisee/be/vardo/OfferRideSuccessActivity.java
@@ -2,12 +2,22 @@ package odisee.be.vardo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OfferRideSuccessActivity extends AppCompatActivity {
+
+    String editFromLocation;
+    String editToLocation;
+    String editOfferDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer_ride_success);
+
+        TextView editFromLocation = findViewById(R.id.editFromLocation);
+        TextView editToLocation = findViewById(R.id.editToLocation);
+        TextView editOfferDateTime = findViewById(R.id.editOfferDateTime);
+
     }
 }
