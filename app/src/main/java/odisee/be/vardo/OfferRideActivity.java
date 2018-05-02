d16b170d225e8a650fdc47a2b5f99e34f4e04294
yawyks <mohamed.dahhou@student.odisee.be>
2018-05-02 12:23:49 +0200

    Implementation of three edit text fields and a button in order to offer a ride. Implementation of a click listener on the button. Activity OfferRideSuccess created.

--- app/src/main/java/odisee/be/vardo/OfferRideActivity.java
+++ app/src/main/java/odisee/be/vardo/OfferRideActivity.java
@@ -1,13 +1,28 @@
 package odisee.be.vardo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OfferRideActivity extends AppCompatActivity {
+
+    Button btnOfferRideNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer_ride);
+
+        btnOfferRideNext = findViewById(R.id.btnOfferRideNext);
+
+        btnOfferRideNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent btnOfferRideNextClick = new Intent(v.getContext(), OfferRideSuccessActivity.class);
+                startActivity(btnOfferRideNextClick);
+            }
+        });
     }
 }
