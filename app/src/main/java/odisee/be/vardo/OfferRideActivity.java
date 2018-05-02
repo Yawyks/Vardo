0bf21a4afa9a8f7348f8ca714fba7bf6853c00b8
yawyks <mohamed.dahhou@student.odisee.be>
2018-05-02 19:45:00 +0200

    Prototype home screen design with transparent buttons.

--- app/src/main/java/odisee/be/vardo/OfferRideActivity.java
+++ app/src/main/java/odisee/be/vardo/OfferRideActivity.java
@@ -5,8 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class OfferRideActivity extends AppCompatActivity {
+
+    private EditText editFromLocation;
+    private EditText editToLocation;
+    private EditText editOfferDateTime;
 
     Button btnOfferRideNext;
 
@@ -15,12 +20,17 @@ public class OfferRideActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer_ride);
 
+        editFromLocation = findViewById(R.id.editFromLocation);
+        editToLocation = findViewById(R.id.editToLocation);
+        editOfferDateTime = findViewById(R.id.editOfferDateTime);
+
         btnOfferRideNext = findViewById(R.id.btnOfferRideNext);
 
         btnOfferRideNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent btnOfferRideNextClick = new Intent(v.getContext(), OfferRideSuccessActivity.class);
+
                 startActivity(btnOfferRideNextClick);
             }
         });
--- app/src/main/java/odisee/be/vardo/OfferRideSuccessActivity.java
+++ app/src/main/java/odisee/be/vardo/OfferRideSuccessActivity.java
@@ -3,28 +3,16 @@ package odisee.be.vardo;
 import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
 
 public class OfferRideSuccessActivity extends AppCompatActivity {
-
-    private EditText editFromLocation;
-    private EditText editToLocation;
-    private EditText editOfferDateTime;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer_ride_success);
-
-        editFromLocation = findViewById(R.id.editFromLocation);
-        editToLocation = findViewById(R.id.editToLocation);
-        editOfferDateTime = findViewById(R.id.editOfferDateTime);
-
-
     }
 }
