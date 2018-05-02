a4ad8f77407dd3426d05d0b1f8a44446c97ba29d
yawyks <mohamed.dahhou@student.odisee.be>
2018-05-02 18:49:11 +0200

    Offer a ride activity has been designed in order to be more user-friendly (colors, icons & padding). Prototype offer a ride succeeded.

--- app/src/main/java/odisee/be/vardo/OfferRideSuccessActivity.java
+++ app/src/main/java/odisee/be/vardo/OfferRideSuccessActivity.java
@@ -1,23 +1,30 @@
 package odisee.be.vardo;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
 
 public class OfferRideSuccessActivity extends AppCompatActivity {
 
-    String editFromLocation;
-    String editToLocation;
-    String editOfferDateTime;
+    private EditText editFromLocation;
+    private EditText editToLocation;
+    private EditText editOfferDateTime;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer_ride_success);
 
-        TextView editFromLocation = findViewById(R.id.editFromLocation);
-        TextView editToLocation = findViewById(R.id.editToLocation);
-        TextView editOfferDateTime = findViewById(R.id.editOfferDateTime);
+        editFromLocation = findViewById(R.id.editFromLocation);
+        editToLocation = findViewById(R.id.editToLocation);
+        editOfferDateTime = findViewById(R.id.editOfferDateTime);
+
 
     }
 }
