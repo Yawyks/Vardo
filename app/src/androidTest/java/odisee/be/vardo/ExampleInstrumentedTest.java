2b37c2ecf54d99e3e681cb21fa852eefa2bb43b4
yawyks <mohamed.dahhou@student.odisee.be>
2018-05-03 22:52:29 +0200

    Renaming background images. Reducing view bars.

--- app/src/androidTest/java/odisee/be/vardo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/odisee/be/vardo/ExampleInstrumentedTest.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 /**
- * Instrumented test, which will execute on an Android device.
+ * Instrumented home_background, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
@@ -18,7 +18,7 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
+        // Context of the app under home_background.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("odisee.be.vardo", appContext.getPackageName());
