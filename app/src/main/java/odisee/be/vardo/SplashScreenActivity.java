999125146e685eec565abc4dacbe80c4729648a9
yawyks <mohamed.dahhou@student.odisee.be>
2018-05-05 20:59:51 +0200

    Splash screen implemented.

---
+++ app/src/main/java/odisee/be/vardo/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package odisee.be.vardo;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            public void run() {
+                final Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                SplashScreenActivity.this.startActivity(i);
+                SplashScreenActivity.this.finish();
+            }
+        }, 5000);
+    }
+}
