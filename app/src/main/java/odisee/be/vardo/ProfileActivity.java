34bf621d29ffbad50a73d78c23481f13ea456480
yawyks <mohamed.dahhou@student.odisee.be>
2018-05-05 21:38:25 +0200

    Bugfix email authentification not updated while in the database it is.

--- app/src/main/java/odisee/be/vardo/ProfileActivity.java
+++ app/src/main/java/odisee/be/vardo/ProfileActivity.java
@@ -8,6 +8,7 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,9 +16,12 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -152,6 +156,17 @@ public class ProfileActivity extends AppCompatActivity {
         myMapUserInformation.put("Phone Number", myUserProfilePhoneNumber);
 
         myUserDatabase.updateChildren(myMapUserInformation);
+
+        FirebaseUser myUser = FirebaseAuth.getInstance().getCurrentUser();
+        myUser.updateEmail(myUserProfileEmail)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+
+                        }
+                    }
+                });
 
         if (myUriAvatarResult != null) {
 
