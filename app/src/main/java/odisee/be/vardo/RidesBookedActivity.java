2430fae2d9bec2453d784581a0bde13d8af751d4
yawyks <mohamed.dahhou@student.odisee.be>
2018-05-23 13:45:01 +0200

    Navigation drawer implemented in activity rides booked.

--- app/src/main/java/odisee/be/vardo/RidesBookedActivity.java
+++ app/src/main/java/odisee/be/vardo/RidesBookedActivity.java
@@ -1,9 +1,15 @@
 package odisee.be.vardo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -17,7 +23,7 @@ import java.util.ArrayList;
 import odisee.be.vardo.ridesBookedRecyclerView.RidesBookedAdapter;
 import odisee.be.vardo.ridesBookedRecyclerView.RidesBookedObject;
 
-public class RidesBookedActivity extends AppCompatActivity {
+public class RidesBookedActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private String myUserId;
 
@@ -28,10 +34,26 @@ public class RidesBookedActivity extends AppCompatActivity {
 
     private ArrayList resultRidesBooked = new ArrayList<RidesBookedObject>();
 
+    // Navigation Drawer
+    private DrawerLayout myDrawerLayout;
+    private ActionBarDrawerToggle myActionBarDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rides_booked);
+
+        // Navigation Drawer
+        myDrawerLayout = findViewById(R.id.navigationDrawer);
+        myActionBarDrawerToggle = new ActionBarDrawerToggle(this, myDrawerLayout, R.string.open, R.string.close);
+
+        myDrawerLayout.addDrawerListener(myActionBarDrawerToggle);
+
+        myActionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView myNavigationView = findViewById(R.id.navigation_content);
+        myNavigationView.setNavigationItemSelectedListener(this);
 
         myRidesBookedRecyclerView = findViewById(R.id.recyclerViewRidesBooked);
         myRidesBookedRecyclerView.setNestedScrollingEnabled(false);
@@ -115,5 +137,51 @@ public class RidesBookedActivity extends AppCompatActivity {
 
     private ArrayList<RidesBookedObject> getRidesBooked() {
         return resultRidesBooked;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (myActionBarDrawerToggle.onOptionsItemSelected(item)) {
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+
+            case R.id.navigationDrawerItemHome:
+                Intent h = new Intent(RidesBookedActivity.this, HomeActivity.class);
+                startActivity(h);
+                break;
+            case R.id.navigationDrawerItemRidesOffered:
+                Intent r = new Intent(RidesBookedActivity.this, RidesOfferedActivity.class);
+                startActivity(r);
+                break;
+            case R.id.navigationDrawerItemRidesBooked:
+                Intent b = new Intent(RidesBookedActivity.this, RidesBookedActivity.class);
+                startActivity(b);
+                break;
+            case R.id.navigationDrawerItemProfile:
+                Intent p = new Intent(RidesBookedActivity.this, ProfileActivity.class);
+                startActivity(p);
+                break;
+            case R.id.navigationDrawerItemCampus:
+                Intent s = new Intent(RidesBookedActivity.this, CampussesActivity.class);
+                startActivity(s);
+                break;
+            case R.id.navigationDrawerItemLogout:
+                FirebaseAuth.getInstance().signOut();
+                Intent l = new Intent(RidesBookedActivity.this, LoginActivity.class);
+                startActivity(l);
+                finish();
+                break;
+        }
+
+        return false;
     }
 }
